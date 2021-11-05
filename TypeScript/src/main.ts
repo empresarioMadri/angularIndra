68902b3d20b528bac3d90cdc2d5d2dde93044b1b
David Robledo <david.robledo@cas-training.com>
2021-11-05 09:01:26 +0100

    Comienzo typescript

--- TypeScript/src/main.ts
+++ TypeScript/src/main.ts
@@ -12,3 +12,16 @@ platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
 
 
+console.log('-------Tipos typescript--------')
+
+function sumar(x:number,y:number){
+  return x+y;
+}
+
+let val1 = (<HTMLInputElement>document.getElementById('value1')).value;
+let val2 = (<HTMLInputElement>document.getElementById('value2')).value;
+
+console.log(sumar(parseInt(val1),parseInt(val2)));
+
+
+
