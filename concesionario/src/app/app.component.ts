756c8c82925efe78cb4384a19c3040276bca278e
David Robledo <david.robledo@cas-training.com>
2021-11-08 13:54:22 +0100

    version con indice estico

--- concesionario/src/app/app.component.ts
+++ concesionario/src/app/app.component.ts
@@ -11,23 +11,32 @@ export class AppComponent {
   marca:string='';
   color:string='';
 
+
   constructor(){
     this.coches = [];
   }
 
   incluirCoche(){
-    const coche = new Coche(this.marca,this.color);
+    const coche = new Coche(this.marca,this.color,Coche.ultimaPosicion);
+    Coche.ultimaPosicion++;
     this.coches.push(coche);
   }
 
   modificar(indice:number){
     const posicion = this.coches[indice];
-    posicion.color = this.color;
-    posicion.marca = this.marca;
+    if(this.marca)
+      posicion.marca = this.marca;
+    if(this.color)
+      posicion.color = this.color;
   }
 
   borrar(indice:number){
     this.coches.splice(indice,1);
+    Coche.ultimaPosicion--;
+    for (let index = 0; index < this.coches.length; index++) {
+      const coche = this.coches[index];
+      coche.posicion=index;
+    }
   }
 
 
@@ -36,10 +45,13 @@ export class AppComponent {
 class Coche{
   private _marca:string;
   private _color:string;
+  private _posicion:number;
+  static ultimaPosicion:number=0;
 
-  constructor(marca:string,color:string){
+  constructor(marca:string,color:string,posicion:number){
     this._marca=marca;
     this._color=color;
+    this._posicion=posicion;
   }
 
   public get marca():string{
@@ -56,6 +68,14 @@ class Coche{
 
   public set color(color:string){
     this._color=color;
+  }
+
+  public get posicion():number{
+    return this._posicion;
+  }
+
+  public set posicion(posicion:number){
+    this._posicion=posicion;
   }
 
 }
