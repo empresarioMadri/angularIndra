28f6196048208373adbf1470540245358a4e9395
David Robledo <david.robledo@cas-training.com>
2021-11-08 12:33:17 +0100

    tabla alta de coches

--- concesionario/src/app/app.component.ts
+++ concesionario/src/app/app.component.ts
@@ -6,25 +6,46 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'concesionario';
-  nombre = 'Proyecto hola mundo inicial';
-  precioCoches =  [1330,500,200];
-  numeroCoches = 0;
+  
+  coches: Coche[];
+  marca:string='';
+  color:string='';
 
-  sumaPrecios():number{
-    let suma =0;
-    this.precioCoches.forEach((num)=>suma+=num);
-    return suma;
+  constructor(){
+    this.coches = [];
   }
 
-  incrementar(){
-    this.numeroCoches++;
-  }
-
-  decrementar(){
-    this.numeroCoches--;
+  incluirCoche(){
+    const coche = new Coche(this.marca,this.color);
+    this.coches.push(coche);
   }
 
 
+}
+
+class Coche{
+  private _marca:string;
+  private _color:string;
+
+  constructor(marca:string,color:string){
+    this._marca=marca;
+    this._color=color;
+  }
+
+  public get marca():string{
+    return this._marca;
+  }
+
+  public get color():string{
+    return this._color;
+  }
+
+  public set marca(marca:string){
+    this._marca=marca;
+  }
+
+  public set color(color:string){
+    this._color=color;
+  }
 
 }
