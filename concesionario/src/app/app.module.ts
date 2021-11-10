90f98493f18162c2b9332536b867e25ab6fa846c
David Robledo <david.robledo@cas-training.com>
2021-11-10 09:43:58 +0100

    componente pie de pagina

--- concesionario/src/app/app.module.ts
+++ concesionario/src/app/app.module.ts
@@ -5,11 +5,13 @@ import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CabeceraComponent } from './cabecera/cabecera.component';
+import { PieComponent } from './pie/pie.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CabeceraComponent
+    CabeceraComponent,
+    PieComponent
   ],
   imports: [
     BrowserModule,
