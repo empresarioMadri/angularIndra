db714985a6cc47d666fbf9fbf1b3af0752e4265c
David Robledo <david.robledo@cas-training.com>
2021-11-11 11:05:59 +0100

    proyecto sin cambios routing

--- concesionario/src/app/app-routing.module.ts
+++ concesionario/src/app/app-routing.module.ts
@@ -1,13 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { MenuComponent } from './menu/menu.component';
 
-const routes: Routes = [
-  {
-    path:'menuPrueba',
-    component:MenuComponent
-  }
-];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
