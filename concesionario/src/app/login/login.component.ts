aeccc62aca8a6cbffc30a661e8ec966b905ed735
David Robledo <david.robledo@cas-training.com>
2021-11-11 11:33:09 +0100

    pipes I

--- concesionario/src/app/login/login.component.ts
+++ concesionario/src/app/login/login.component.ts
@@ -7,6 +7,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
+  nombre:string = 'David';
+  saldo = 150.5;
+  dias=['lunes','martes','miercoles','jueves','viernes','sabado','domingo'];
+
+  coches=[
+    {marca:'seat',color:'rojo'},
+    {marca:'opel',color:'azul'}
+  ]
+
+  fechaActual = new Date();
+
   constructor() { }
 
   ngOnInit(): void {
