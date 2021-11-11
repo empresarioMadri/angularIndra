825edd5ab25a0d42732f21b785ce9e57883a5bab
David Robledo <david.robledo@cas-training.com>
2021-11-11 11:12:34 +0100

    incluido componente inicio

--- concesionario/src/app/app.component.ts
+++ concesionario/src/app/app.component.ts
@@ -1,6 +1,4 @@
 import { Component, ViewChild } from '@angular/core';
-import { CabeceraComponent } from './cabecera/cabecera.component';
-import { CochesService } from './coches.service';
 
 @Component({
   selector: 'app-root',
@@ -8,103 +6,6 @@ import { CochesService } from './coches.service';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  
-  coches: any;
-  marca:string='';
-  color:string='';
-  cadenaPie:string ='';
-
-  @ViewChild('selector1')
-  selector1!: CabeceraComponent;
-
-  ngOnInit(){
-    this.cocheServicio.traerCoches().subscribe(
-      result=>{
-        this.coches=result;
-      },
-      error=>{
-        console.log('Error al recuperar coches');
-      }
-    )
-  }
-
-
-  constructor(private cocheServicio:CochesService){
-    this.coches = [];
-  }
-
-  incluirCoche(){
-    const coche = new Coche(this.marca,this.color,Coche.ultimaPosicion);
-    Coche.ultimaPosicion++;
-    this.coches.push(coche);
-  }
-
-  modificar(indice:number){
-    const posicion = this.coches[indice];
-    if(this.marca)
-      posicion.marca = this.marca;
-    if(this.color)
-      posicion.color = this.color;
-  }
-
-  borrar(indice:number){
-    this.coches.splice(indice,1);
-    Coche.ultimaPosicion--;
-    for (let index = 0; index < this.coches.length; index++) {
-      const coche = this.coches[index];
-      coche.posicion=index;
-    }
-  }
-
-  cambiarPie(cadena:string){
-    this.cadenaPie=cadena;
-  }
-
-  incrementar(){
-    this.selector1.incrementar();
-  }
-
-  decrementar(){
-    this.selector1.decrementar();
-  }
-
 
 }
 
-class Coche{
-  private _marca:string;
-  private _color:string;
-  private _posicion:number;
-  static ultimaPosicion:number=0;
-
-  constructor(marca:string,color:string,posicion:number){
-    this._marca=marca;
-    this._color=color;
-    this._posicion=posicion;
-  }
-
-  public get marca():string{
-    return this._marca;
-  }
-
-  public get color():string{
-    return this._color;
-  }
-
-  public set marca(marca:string){
-    this._marca=marca;
-  }
-
-  public set color(color:string){
-    this._color=color;
-  }
-
-  public get posicion():number{
-    return this._posicion;
-  }
-
-  public set posicion(posicion:number){
-    this._posicion=posicion;
-  }
-
-}
--- concesionario/src/app/app.module.ts
+++ concesionario/src/app/app.module.ts
@@ -8,12 +8,14 @@ import { CabeceraComponent } from './cabecera/cabecera.component';
 import { PieComponent } from './pie/pie.component';
 import { ConcesionarioModule } from './concesionario/concesionario.module';
 import { HttpClientModule } from '@angular/common/http';
+import { InicioComponent } from './inicio/inicio.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CabeceraComponent,
-    PieComponent
+    PieComponent,
+    InicioComponent
   ],
   imports: [
     BrowserModule,
