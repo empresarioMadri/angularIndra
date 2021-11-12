fd6d371c51d9fcdc12e187b8b604d3280ca54d9d
David Robledo <david.robledo@cas-training.com>
2021-11-12 13:55:01 +0100

    Incluido lombok

---
+++ ProyectoHolaMundo/src/main/java/com/indra/springboot/ProyectoHolaMundo/controllers/Coche.java
@@ -0,0 +1,13 @@
+package com.indra.springboot.ProyectoHolaMundo.controllers;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+@AllArgsConstructor
+public class Coche {
+    private String marca;
+    private String color;
+}
