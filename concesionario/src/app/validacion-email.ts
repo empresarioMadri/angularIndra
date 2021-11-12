83ffe31d7f27833b385f221a53ee38b398d1da1e
David Robledo <david.robledo@cas-training.com>
2021-11-12 08:14:05 +0100

    validacion personalizada

--- concesionario/src/app/validacion-email.ts
+++ concesionario/src/app/validacion-email.ts
@@ -1,11 +1,11 @@
-import { FormControl, ValidationErrors } from "@angular/forms";
+import { AbstractControl, ValidationErrors } from "@angular/forms";
 
 export class ValidacionEmail {
-    static validar(control:FormControl):ValidationErrors{
+    static validar(control:AbstractControl):ValidationErrors{
         if(!control.value){
             return {validar:false};
         }else{
-            return {validar:control.value.indexOf('@')>=0};
+            return {validar:control.value.indexOf('@')<0};
         }
     }
 }
