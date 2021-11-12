27814649449e855e2b94ed252341b5698fc07cda
David Robledo <david.robledo@cas-training.com>
2021-11-12 12:03:32 +0100

    Proyecto Inicial spring boot

---
+++ ProyectoHolaMundo/src/main/java/com/indra/springboot/ProyectoHolaMundo/ProyectoHolaMundoApplication.java
@@ -0,0 +1,13 @@
+package com.indra.springboot.ProyectoHolaMundo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoHolaMundoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoHolaMundoApplication.class, args);
+	}
+
+}
