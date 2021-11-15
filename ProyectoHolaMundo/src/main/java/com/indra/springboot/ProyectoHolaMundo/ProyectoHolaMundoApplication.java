6cce752e1dc83a3fd389cf093f21cd6b29d26c2d
David Robledo <david.robledo@cas-training.com>
2021-11-15 10:06:21 +0100

    cambios entre idiomas

--- ProyectoHolaMundo/src/main/java/com/indra/springboot/ProyectoHolaMundo/ProyectoHolaMundoApplication.java
+++ ProyectoHolaMundo/src/main/java/com/indra/springboot/ProyectoHolaMundo/ProyectoHolaMundoApplication.java
@@ -5,9 +5,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Description;
 import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+
+import java.util.Locale;
 
 @SpringBootApplication
 public class ProyectoHolaMundoApplication implements WebMvcConfigurer {
@@ -34,6 +38,14 @@ public class ProyectoHolaMundoApplication implements WebMvcConfigurer {
 	@Override
 	public void addInterceptors(InterceptorRegistry registry) {
 		registry.addInterceptor(localeChangeInterceptor());
+	}
+
+	@Bean
+	public LocaleResolver localeResolver()
+	{
+		final SessionLocaleResolver localeResolver = new SessionLocaleResolver();
+		localeResolver.setDefaultLocale(new Locale("en", "UK"));
+		return localeResolver;
 	}
 
 }
