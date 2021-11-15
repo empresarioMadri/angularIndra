ce521efd3d253ecceb4cab91ff3c9653a363dc55
David Robledo <david.robledo@cas-training.com>
2021-11-15 10:00:31 +0100

    internacionalizacion

--- ProyectoHolaMundo/src/main/java/com/indra/springboot/ProyectoHolaMundo/ProyectoHolaMundoApplication.java
+++ ProyectoHolaMundo/src/main/java/com/indra/springboot/ProyectoHolaMundo/ProyectoHolaMundoApplication.java
@@ -2,12 +2,38 @@ package com.indra.springboot.ProyectoHolaMundo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Description;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 
 @SpringBootApplication
-public class ProyectoHolaMundoApplication {
+public class ProyectoHolaMundoApplication implements WebMvcConfigurer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProyectoHolaMundoApplication.class, args);
+	}
+
+	@Bean
+	@Description("Spring Message Resolver")
+	public ResourceBundleMessageSource messageSource() {
+		ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+		messageSource.setBasename("mensajes");
+		return messageSource;
+	}
+
+	@Bean
+	public LocaleChangeInterceptor localeChangeInterceptor() {
+		LocaleChangeInterceptor lci = new LocaleChangeInterceptor();
+		lci.setParamName("lang");
+		return lci;
+	}
+
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		registry.addInterceptor(localeChangeInterceptor());
 	}
 
 }
