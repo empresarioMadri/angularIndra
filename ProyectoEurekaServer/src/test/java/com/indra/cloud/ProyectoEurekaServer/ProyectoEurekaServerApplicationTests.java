bcffb748731298b785ed8062b639a3825fa4d1fe
David Robledo <david.robledo@cas-training.com>
2021-11-24 10:51:35 +0100

    spring cloud eureka

---
+++ ProyectoEurekaServer/src/test/java/com/indra/cloud/ProyectoEurekaServer/ProyectoEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.indra.cloud.ProyectoEurekaServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
