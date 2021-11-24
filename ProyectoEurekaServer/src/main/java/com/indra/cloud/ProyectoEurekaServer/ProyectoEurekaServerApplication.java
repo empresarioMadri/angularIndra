b7adce6db36f4e13434bba425e15cc3338ab5552
David Robledo <david.robledo@cas-training.com>
2021-11-24 10:56:57 +0100

    eureka server cloud II

--- ProyectoEurekaServer/src/main/java/com/indra/cloud/ProyectoEurekaServer/ProyectoEurekaServerApplication.java
+++ ProyectoEurekaServer/src/main/java/com/indra/cloud/ProyectoEurekaServer/ProyectoEurekaServerApplication.java
@@ -2,8 +2,10 @@ package com.indra.cloud.ProyectoEurekaServer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class ProyectoEurekaServerApplication {
 
 	public static void main(String[] args) {
