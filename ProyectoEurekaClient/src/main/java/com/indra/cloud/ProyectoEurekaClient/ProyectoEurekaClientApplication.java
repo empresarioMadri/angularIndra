4a0684b3a399841dbcd2f0ef2bb4d08a4d224051
David Robledo <david.robledo@cas-training.com>
2021-11-24 12:10:48 +0100

    cambios eureka holaMundoClient

--- ProyectoEurekaClient/src/main/java/com/indra/cloud/ProyectoEurekaClient/ProyectoEurekaClientApplication.java
+++ ProyectoEurekaClient/src/main/java/com/indra/cloud/ProyectoEurekaClient/ProyectoEurekaClientApplication.java
@@ -2,8 +2,10 @@ package com.indra.cloud.ProyectoEurekaClient;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class ProyectoEurekaClientApplication {
 
 	public static void main(String[] args) {
--- ProyectoEurekaClient/src/main/java/com/indra/cloud/ProyectoEurekaClient/controllers/ClienteController.java
+++ ProyectoEurekaClient/src/main/java/com/indra/cloud/ProyectoEurekaClient/controllers/ClienteController.java
@@ -1,14 +1,26 @@
 package com.indra.cloud.ProyectoEurekaClient.controllers;
 
+import java.util.List;
+import java.util.Random;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ClienteController {
+	
+	@Autowired
+	private DiscoveryClient discoveryClient;
 
 	@GetMapping("/")
 	public String inicio() {
-		return "Hola mundo";
+		List<ServiceInstance> instances = discoveryClient.getInstances("HolaMundoClient");
+        ServiceInstance serviceInstance = instances.get(new Random().nextInt(instances.size()));
+        return "Hola mundo client:" + serviceInstance.getServiceId() + ":"
+                + serviceInstance.getHost() + ":" + serviceInstance.getPort();
 	}
 
 }
